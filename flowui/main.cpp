8a37f6ac0f5b3e697d0d52c315ff0d1c2e796bae
red939 <k.lihong07@gmail.com>
2026-08-12 19:54:56 +0900

    Apply new icontoon multi-resolution icon set and sync v1.4_stable tag

--- flowui/main.cpp
+++ flowui/main.cpp
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QString iconPath = QCoreApplication::applicationDirPath() + "/icon_data/icon5.ico";
+    QString iconPath = QCoreApplication::applicationDirPath() + "/icontoon/icontoon.ico";
     if (QFile::exists(iconPath)) {
         app.setWindowIcon(QIcon(iconPath));
-    } else if (QFile::exists("icon_data/icon5.ico")) {
-        app.setWindowIcon(QIcon("icon_data/icon5.ico"));
-    } else if (QFile::exists("../icon_data/icon5.ico")) {
-        app.setWindowIcon(QIcon("../icon_data/icon5.ico"));
+    } else if (QFile::exists("icontoon/icontoon.ico")) {
+        app.setWindowIcon(QIcon("icontoon/icontoon.ico"));
+    } else if (QFile::exists("../icontoon/icontoon.ico")) {
+        app.setWindowIcon(QIcon("../icontoon/icontoon.ico"));
     }
 
     QQmlApplicationEngine engine;
